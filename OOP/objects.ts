27daf41cfe5e4fc2c7fb2b9d13f844df16eca153
Inseob Jeon <sonofhuman20@gmail.com>
2021-02-13 08:12:00 +0900

    Making MyString class is finished

--- OOP/objects.ts
+++ OOP/objects.ts
@@ -1,29 +1,44 @@
 // make String() constructor by myself!
-
 class MyString {
-  private inputValue: string[];
+  private inputValue: any;
+  private stringifiedInputValue: any;
+  private converToArr: any;
 
-  constructor(inputValue: any) {
-    this.inputValue = [...`${inputValue}`];
+  constructor(inputvalue: any) {
+    this.inputValue = inputvalue;
+    this.stringifiedInputValue = `${inputvalue}`;
+    this.converToArr = [...this.stringifiedInputValue];
   }
 
   toString(): string {
-    return this.inputValue.join('');
+    return this.stringifiedInputValue;
   }
 
-  charAt(index: number): string {
-    return this.inputValue[index];
+  charAt(index: number): any {
+    return this.converToArr[index];
   }
 
   concat(anotherInput: string): string {
-    return `${this.inputValue}${anotherInput}`;
+    return `${this.stringifiedInputValue}${anotherInput}`;
   }
 
   slice(startIndex, endIndex): string {
     let strToReturn: string = '';
     for (let i = startIndex; i < endIndex; i++) {
-      strToReturn = strToReturn + this.inputValue[i];
+      strToReturn = strToReturn + this.converToArr[i];
     }
     return strToReturn;
-  }  
-}
+  }
+  
+  valueOf(): string {
+    return this.inputValue;
+  }
+};
+
+const test = new MyString(123);
+console.log(
+  test,
+  test.charAt(1),
+  test.slice(0, 1),
+  test.valueOf()
+);
